31bef1b574ad3660c7b184b00b2d51d07f2ca2cc
xiaochengphp <1427514105@qq.com>
2018-03-16 01:19:30 +0800

    Add files via upload

---
+++ pages/wdqx/wdqx.js
@@ -0,0 +1,76 @@
+// pages/wdqx/wdqx.js
+import data from 'data';
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.setData({
+            lists: data
+        });
+    },
+    showDec(op){
+        let id = op.currentTarget.dataset.id;
+
+        wx.navigateTo({
+            url: 'dec/dec?id=' + id,
+        })
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
